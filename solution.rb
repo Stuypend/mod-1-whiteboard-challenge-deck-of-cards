9b1c493636b4a71e879a3445ec30aa615921cb96
stuypend <andre@stuypend.com>
2021-03-08 09:18:47 -0500

    commit tree 279ca78c11be34b31086c5b190e9a67c58e48765

--- solution.rb
+++ solution.rb
@@ -1,7 +1,27 @@
 class Deck
 
+  attr_accessor :cards
+
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def choose_card
+    rng = Random.new()
+    @cards.delete_at(rng.rand(53))
+  end
+
 end
 
 class Card
 
+  attr_accessor :rank, :suit
+
+  def initialize(rank, suit)
+    @rank = rank
+    @suit = suit
+  end
+
 end
+
+
